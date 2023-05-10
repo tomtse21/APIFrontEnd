eaca4c52786e0c50ac915e20c1a5bd990e4f1f18
Ho Fai Tse <tom@HodeMac-mini.local>
2023-05-11 01:13:01 +0800

    added more filed for create new cat

--- src/components/NewCat.tsx
+++ src/components/NewCat.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button, Upload, UploadProps, message, MenuProps, Space, Dropdown, Select } from 'antd';
 import { Buffer } from 'buffer';
 import axios from "axios";
 import { api } from './common/http-common';
 import authHeader from "../services/authHeader";
 import UserConfig from "./common/user-config";
+import { DownOutlined, UploadOutlined } from "@ant-design/icons";
 
 const { TextArea } = Input
 
@@ -12,6 +13,7 @@ const NewCat = () => {
 
 
   const handleFormSubmit = (values: any) => {
+    console.log(values)
     const _name = values.name;
     const _description = values.description;
     const postCat = {
@@ -20,7 +22,7 @@ const NewCat = () => {
     }
 
     // Post request
-    axios.post(`${api.uri}/cats`, postCat, {
+    axios.post(`${api.uri}/cats`, values, {
       headers: 
         authHeader()
     }).then((res) => {
@@ -33,17 +35,61 @@ const NewCat = () => {
     { required: true, message: 'Please input somethings' }
   ]
 
+  const props: UploadProps = {
+    name: 'file',
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    headers: {
+      authorization: 'authorization-text',
+    },
+    onChange(info) {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
+
+  
   return (
-    <Form name="article" onFinish={(values) => handleFormSubmit(values)}>
+    <Form name="article" onFinish={(values) => handleFormSubmit(values)} labelCol={{ span: 9 }}
+    wrapperCol={{ span: 16 }}>
       <Form.Item name="name" label="Name" rules={contentRules}>
+        <Input />
+      </Form.Item>
+      <Form.Item name="age" label="Age" >
+        <Input />
+      </Form.Item>
+     
+      <Form.Item label="Select" name="color">
+          <Select>
+            <Select.Option value="Red">Red</Select.Option>
+            <Select.Option value="Orange">Orange</Select.Option>
+            <Select.Option value="Yellow">Yellow</Select.Option>
+            <Select.Option value="White">White</Select.Option>
+            <Select.Option value="Black">Black</Select.Option>
+            <Select.Option value="Grey">Grey</Select.Option>
+            <Select.Option value="Brown">Brown</Select.Option>
+          </Select>
+        </Form.Item>
+        
+      <Form.Item name="foundlocation" label="Found Location">
         <Input />
       </Form.Item>
       <Form.Item name="description" label="Description" >
         <TextArea rows={4} />
-      </Form.Item>
+      </Form.Item>      
+      <Upload {...props}>
+        <Button icon={<UploadOutlined />}>Click to Upload</Button>
+      </Upload>
+      <p></p>
       <Form.Item>
         <Button type="primary" htmlType="submit">Submit</Button>
       </Form.Item>
+
 
     </Form>
   )
--- src/components/ShowCats.tsx
+++ src/components/ShowCats.tsx
@@ -17,7 +17,7 @@ const Cat = () => {
   const [filteredData, setFilteredData] = React.useState<any[]>([]);
   const [error, setError] = React.useState("");
   const [input, setInput] = React.useState("");
-
+  var unkown = "Unkown";
   const showModal = () => {
     setIsModalOpen(true);
   };
@@ -118,7 +118,10 @@ const Cat = () => {
             filteredData && filteredData!.map((currElement,index) => (
               <Col xs={24} sm={18} md={12} xl={6} key={currElement.id}>
                 <Card style={{paddingLeft: 15, paddingRight: 15}} title={`Name :`+currElement.name} cover={<img alt="example" src="https://cdn2.thecatapi.com/images/8ru.jpg" />}>
-                  <p>Description: {currElement.description}</p>
+                  <p>Age: {currElement.age?currElement.age:unkown}</p>
+                  <p>Color: {currElement.color?currElement.color:unkown}</p>
+                  <p>Description: {currElement.description?currElement.description:unkown}</p>
+                  <p>Found location: {currElement.foundlocation?currElement.foundlocation:unkown}</p>
                   <p></p>
                   {/* <Link to={`/a/${id}`}>Details</Link> */}
                   <p></p>
