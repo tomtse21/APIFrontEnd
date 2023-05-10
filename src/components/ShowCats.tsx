b02a58864230438ee758ea072d1dbf0557950f68
hohopk2001 <takeaerst21@gmail.com>
2023-05-10 17:48:49 +0800

    search done

--- src/components/ShowCats.tsx
+++ src/components/ShowCats.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react';
 import { Link } from 'react-router-dom';
-import { Affix, Button, Card, Col, Input, Row, Space, Spin } from 'antd';
+import { Affix, Button, Card, Col, Input, Modal, Row, Space, Spin } from 'antd';
 import { api } from './common/http-common';
 import axios from 'axios';
 import Icon, { DeleteOutlined, EditOutlined, GithubOutlined, HeartFilled, HeartOutlined, LoadingOutlined, MessageOutlined } from '@ant-design/icons';
@@ -12,9 +12,40 @@ const Cat = () => {
   const [loading, setLoading] = React.useState(true);
   const [isliked, setIsLiked] = React.useState(false);
   const [deleteId, setDeleteId] = React.useState(null);
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const [targetUpdateCat , setTargetUpdateCat] = React.useState({})
+  const [filteredData, setFilteredData] = React.useState<any[]>([]);
+  const [error, setError] = React.useState("");
+  const [input, setInput] = React.useState("");
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   const { Search } = Input;
   const onSearch = (value: string) => console.log(value);
+  const handleFilter = (e:any) => {
+    const searchInput = e.target.value;
+    setInput(searchInput);
+    const newFilterName  = cats.filter((value) => {
+      return       value.name.toLowerCase().includes(searchInput.toLowerCase());
+    });
+    if (searchInput === "") {
+      setFilteredData(cats);
+    } else {
+      setFilteredData(newFilterName);
+    }
+  };
+  
+
   const isAuthenticated = useIsAuthenticated()
   
   React.useEffect(() => {
@@ -26,6 +57,7 @@ const Cat = () => {
     .then((res) => {
       if(res.data.length>=1){
         setCats(res.data);
+        setFilteredData(res.data)
       }
       
     })
@@ -60,8 +92,9 @@ const Cat = () => {
     })
   },[]);
 
-  const onClickUpdate=(id:any)=>{
-    alert(id);
+  const onClickUpdate=(obj:any)=>{
+    setTargetUpdateCat(obj)
+    showModal()
   }
 
   if (loading) {
@@ -75,29 +108,29 @@ const Cat = () => {
       return (
         <>
             <Affix offsetTop={15} >
-                <Row style={{justifyContent:'right'}} ><Search placeholder="input search text" onSearch={onSearch} style={{ width: '200px' }} /></Row>
+                <Row style={{justifyContent:'right'}} ><Search placeholder="input search text" onChange={(e) => handleFilter(e)} onSearch={onSearch} style={{ width: '200px' }} /></Row>
             </Affix>
             
             <p></p>
             <Row gutter={[8, 8]}>
           
           {
-            cats && cats!.map(({ id, name, description }) => (
-              <Col xs={24} sm={18} md={12} xl={6} key={id}>
-                <Card style={{paddingLeft: 15, paddingRight: 15}} title={`Name :`+name} cover={<img alt="example" src="https://cdn2.thecatapi.com/images/8ru.jpg" />}>
-                  <p>Description: {description}</p>
+            filteredData && filteredData!.map((currElement,index) => (
+              <Col xs={24} sm={18} md={12} xl={6} key={currElement.id}>
+                <Card style={{paddingLeft: 15, paddingRight: 15}} title={`Name :`+currElement.name} cover={<img alt="example" src="https://cdn2.thecatapi.com/images/8ru.jpg" />}>
+                  <p>Description: {currElement.description}</p>
                   <p></p>
                   {/* <Link to={`/a/${id}`}>Details</Link> */}
                   <p></p>
                   
                   <Space size='small'>
                       {
-                        isliked? <HeartFilled onClick={()=> onClick(id,false)} /> 
-                        :<HeartOutlined onClick={()=> onClick(id, true)}/>
+                        isliked? <HeartFilled onClick={()=> onClick(currElement.id,false)} /> 
+                        :<HeartOutlined onClick={()=> onClick(currElement.id, true)}/>
                       }
                       <MessageOutlined />
-                      {isAuthenticated()?<EditOutlined onClick={()=> onClickUpdate(id)}/>:null}
-                      <GithubOutlined onClick={()=> onClickDelete(id)}/>
+                      {isAuthenticated()?<EditOutlined onClick={()=> onClickUpdate(currElement)}/>:null}
+                      <GithubOutlined onClick={()=> onClickDelete(currElement.id)}/>
                   </Space>
                    
                 </Card>
@@ -105,6 +138,9 @@ const Cat = () => {
             ))
           }
         </Row>
+        <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+            
+        </Modal>
         </>
         
       )
