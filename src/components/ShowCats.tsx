7dc1bd352ea1bd3cf4457e72d3f22f8353ffb2b2
Ho Fai Tse <tom@HodeMac-mini.local>
2023-05-14 21:27:14 +0800

    can update form data now

--- src/components/ShowCats.tsx
+++ src/components/ShowCats.tsx
@@ -1,12 +1,14 @@
 import React, { useCallback } from 'react';
 import { Link } from 'react-router-dom';
-import { Affix, Button, Card, Col, Input, Modal, Row, Space, Spin } from 'antd';
+import { Affix, Alert, Button, Card, Col, Form, Input, Modal, Row, Select, Space, Spin, Upload } from 'antd';
 import { api } from './common/http-common';
 import axios from 'axios';
 import Icon, { DeleteOutlined, EditOutlined, GithubOutlined, HeartFilled, HeartOutlined, LoadingOutlined, MessageOutlined } from '@ant-design/icons';
 import { useIsAuthenticated } from 'react-auth-kit';
 import authHeader from "../services/authHeader";
 import {Buffer} from 'buffer';
+import TextArea from 'antd/es/input/TextArea';
+import { RcFile } from 'antd/es/upload';
 
 const Cat = () => {
   const [cats, setCats] = React.useState<any[]>([])
@@ -19,9 +21,17 @@ const Cat = () => {
   const [error, setError] = React.useState("");
   const [input, setInput] = React.useState("");
   const [b64, setb64] = React.useState("");
+  const [showMessage, setShowMessage] = React.useState(false);
+  const [successStr, setSuccessStr] = React.useState("");
+  const [statusSuccess, setStatusSuccess] = React.useState(false);
+
+  const contentRules = [
+    { required: true, message: 'Please input somethings' }
+  ]
 
   var unkown = "Unkown";
   const showModal = () => {
+    setShowMessage(false);
     setIsModalOpen(true);
   };
 
@@ -48,7 +58,7 @@ const Cat = () => {
     }
   };
   
-  
+  const [ form ] = Form.useForm()
 
   const isAuthenticated = useIsAuthenticated()
   
@@ -62,8 +72,8 @@ const Cat = () => {
       if(res.data.length>=1){
         setCats(res.data);
         setFilteredData(res.data)
-        console.log(res.data[0].imageuri['data'], 'base64')
-        setb64(Buffer.from(res.data[0].imageuri['data']).toString('base64'))
+        // console.log(res.data[0].imageuri['data'], 'base64')
+        // setb64(Buffer.from(res.data[0].imageuri['data']).toString('base64'))
       }
       
     })
@@ -97,12 +107,31 @@ const Cat = () => {
       setLoading(false);
     })
   },[]);
-
-  const onClickUpdate=(obj:any)=>{
-    setTargetUpdateCat(obj)
+  
+  const onClickUpdate=(obj:any,id: any)=>{
+    form.setFieldsValue(obj)
     showModal()
   }
 
+  const handleFormSubmit =  async (values: any) => {
+    axios.put(`${api.uri}/cats/${values.id}`, values, {
+      headers: 
+        authHeader()
+      }).then((res) => {
+        setShowMessage(true);
+        if (res.status == 201) {
+          setStatusSuccess(true);
+          setSuccessStr("Create successfully!")
+          initPageGetData();
+        }
+      }).catch(function(error) {
+        setShowMessage(true);
+        setSuccessStr("login failed, please insert corrent user information!")
+      });
+
+  }
+
+  
   if (loading) {
     const antIcon = <LoadingOutlined style={{ fontSize: 48 }} spin />
     return (
@@ -118,12 +147,13 @@ const Cat = () => {
             </Affix>
             
             <p></p>
-            <Row gutter={[8, 8]}>
+            <Row gutter={{ xs: 24, sm: 18, md: 12, lg: 6 , xl:3}}>
 
           {
             filteredData && filteredData!.map((currElement,index) => (
-              <Col xs={24} sm={18} md={12} xl={6} key={currElement.id}>
-                <Card style={{paddingLeft: 15, paddingRight: 15}} title={`Name :`+currElement.name} cover={<img  src='' />}>
+
+              <Col xs={24} sm={18} md={12} lg={6} xl ={3} key={currElement.id}>
+                <Card  style={{height:350}}  title={`Name :`+currElement.name} cover={<img  src='' />}>
                   <p>Age: {currElement.age?currElement.age:unkown}</p>
                   <p>Color: {currElement.color?currElement.color:unkown}</p>
                   <p>Description: {currElement.description?currElement.description:unkown}</p>
@@ -138,7 +168,7 @@ const Cat = () => {
                         :<HeartOutlined onClick={()=> onClick(currElement.id, true)}/>
                       }
                       <MessageOutlined />
-                      {isAuthenticated()?<EditOutlined onClick={()=> onClickUpdate(currElement)}/>:null}
+                      {isAuthenticated()?<EditOutlined onClick={()=> onClickUpdate(currElement,currElement.id)}/>:null}
                       <GithubOutlined onClick={()=> onClickDelete(currElement.id)}/>
                   </Space>
                    
@@ -147,8 +177,49 @@ const Cat = () => {
             ))
           }
         </Row>
-        <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}  footer={[]} >
+          {showMessage && (
+              <Alert message={successStr} type={statusSuccess ? "success" : "error"} closable />
+          )}
+            <p></p>
+          <Form name="article" onFinish={(values) => handleFormSubmit(values)} labelCol={{ span: 9 }} form={form}
+            wrapperCol={{ span: 16 }} >
+              <Form.Item name="id" hidden >
+                <Input />
+              </Form.Item>
+              <Form.Item name="name" label="Name" rules={contentRules}>
+                <Input />
+              </Form.Item>
+              <Form.Item name="age" label="Age" >
+                <Input />
+              </Form.Item>
             
+              <Form.Item label="Select" name="color">
+                  <Select>
+                    <Select.Option value="Red">Red</Select.Option>
+                    <Select.Option value="Orange">Orange</Select.Option>
+                    <Select.Option value="Yellow">Yellow</Select.Option>
+                    <Select.Option value="White">White</Select.Option>
+                    <Select.Option value="Black">Black</Select.Option>
+                    <Select.Option value="Grey">Grey</Select.Option>
+                    <Select.Option value="Brown">Brown</Select.Option>
+                  </Select>
+                </Form.Item>
+                
+              <Form.Item name="foundlocation" label="Found Location">
+                <Input />
+              </Form.Item>
+              <Form.Item name="description" label="Description" >
+                <TextArea rows={4} />
+              </Form.Item>      
+              
+              <p></p>
+              <Form.Item  className="align-right">
+                <Button type="primary" htmlType="submit">Submit</Button>
+              </Form.Item>
+
+
+            </Form>
         </Modal>
         </>
         
