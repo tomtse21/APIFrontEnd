8c0d656f265b61556ce6a56e3dbbacaa32ec7f74
Ho Fai Tse <tom@HodeMac-mini.local>
2023-05-18 01:43:37 +0800

    config file

--- src/components/common/http-common.ts
+++ src/components/common/http-common.ts
@@ -1,5 +1,6 @@
 export const api = {
   uri: "http://localhost:10888/api/v1",
+  // uri: "http://192.168.8.5:10888/api/v1",
   //uri: "https://apibackend.227021367.repl.co/api/v1",
   catApiKey: "live_cPeQF89boYbXcZqNrLPL6WrllRWpyNkSpGon8hmJsBeZUNrCewPgPeecj469stFj"
 }
